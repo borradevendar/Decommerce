4dbbcd928456231d9822adee93ce4f1a68cfd0c3
borradevendar <108571547+borradevendar@users.noreply.github.com>
2024-02-18 16:52:29 +0530

    added Products

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import {Routes,Route} from 'react-router-dom'
-import HomePage from './pages/HomePage';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Login from "../src/pages/Auth/Login"
-import Policy from './pages/Policy';
-import Pagenotfound from './pages/Pagenotfound';
-import Register from './pages/Auth/Register';
-import 'react-toastify/dist/ReactToastify.css';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "../src/pages/Auth/Login";
+import Policy from "./pages/Policy";
+import Pagenotfound from "./pages/Pagenotfound";
+import Register from "./pages/Auth/Register";
 function App() {
   return (
     <>
-        <Routes>
-          <Route path='/' element={<HomePage/>}/>
-          <Route path='/register' element={<Register/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-          <Route path='/policy' element={<Policy/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='*' element={<Pagenotfound/>}/>
-        </Routes>
-
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Pagenotfound />} />
+      </Routes>
     </>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../components/layout/Layout";
+import axios from 'axios';
 const HomePage = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get("/api/v1/auth/products");
+        setProducts(response.data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <Layout title={"Home - Decommerce app"}>
-      <h1>HomePage</h1>
+      <div>
+        <h2>Product List</h2>
+        <ul>
+        {products.map((product) => (
+          <li key={product._id}>
+            <img src={product.imageUrl} alt={product.name} />
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+          </li>
+        ))}
+      </ul>
+      </div>
     </Layout>
   );
 };
